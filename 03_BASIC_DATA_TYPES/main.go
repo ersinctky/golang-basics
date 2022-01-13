5dcba23d40081a2fa78b81711e274df93d1616da
Ersin Çatkaya <ersin_ctky@hotmail.com>
2022-01-13 15:09:00 +0300

    Basic Data Types

---
+++ 03_BASIC_DATA_TYPES/main.go
@@ -0,0 +1,27 @@
+// Package Clause
+package main
+
+// Import Statement
+import "fmt"
+
+// My Codes
+func main() {
+
+	var name = "Ersin"
+
+	var age uint8 = 30
+
+	var isMarried bool = false
+
+	var weight float32 = 90
+
+	fmt.Println(name)
+	fmt.Println(age)
+	fmt.Println(isMarried)
+	fmt.Println(weight)
+
+	fmt.Printf("%T\n", name)
+	fmt.Printf("%T\n", age)
+	fmt.Printf("%T\n", isMarried)
+	fmt.Printf("%T\n", weight)
+}
